1e0808ee0ab573891fa78513cfab61a91beb556e
david khurtsidze <david_khurtsidze@epam.com>
2022-11-22 19:36:47 +0400

    renamed exception class

---
+++ Domain/Exceptions/SampleNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using Domain.Exceptions.Base;
+
+namespace Domain.Exceptions
+{
+    public sealed class SampleNotFoundException : NotFoundException
+    {
+        public SampleNotFoundException(Guid id) : base($"The {nameof(Sample)} with the identifier {id} was not found.")
+        {
+
+        }
+    }
+}
